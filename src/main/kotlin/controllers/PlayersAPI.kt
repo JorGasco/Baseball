eafcc65ff3aeff7981ecdada2c9a00068ada38bd
Jorge <20099103@mail.wit.ie>
2023-05-01 01:04:50 +0100

    Add player

---
+++ src/main/kotlin/controllers/PlayersAPI.kt
@@ -0,0 +1,15 @@
+package controllers
+
+import models.Players
+import persistence.Serializer
+
+class PlayersAPI(serializerType: Serializer) {
+
+    private var serializer: Serializer = serializerType
+
+    private var players = ArrayList<Players>()
+
+    fun add(p: Players): Boolean {
+        return this.players.add(p)
+    }
+}
